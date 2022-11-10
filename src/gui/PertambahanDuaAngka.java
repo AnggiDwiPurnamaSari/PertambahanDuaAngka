652e315b832fe52b654aa5519a5361fb5539d017
AnggiDwiPurnamaSari <117338411+AnggiDwiPurnamaSari@users.noreply.github.com>
2022-11-10 13:04:19 +0800

    membuat kode program hapus

--- src/gui/PertambahanDuaAngka.java
+++ src/gui/PertambahanDuaAngka.java
@@ -131,6 +131,11 @@ public class PertambahanDuaAngka extends javax.swing.JFrame {
         });
 
         hapusBtn.setText("Hapus");
+        hapusBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                hapusBtnActionPerformed(evt);
+            }
+        });
 
         keluarBtn.setText("Keluar");
 
@@ -201,6 +206,15 @@ public class PertambahanDuaAngka extends javax.swing.JFrame {
             hasilTF.setText(Integer.toString(hasil));
         }
     }//GEN-LAST:event_tambahBtnActionPerformed
+
+    private void hapusBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_hapusBtnActionPerformed
+        // TODO add your handling code here:
+        angkaPertamaTF.setText("");
+        angkaKeduaTF.setText(null);
+        hasilTF.setText("");
+        
+        angkaPertamaTF.requestFocus();
+    }//GEN-LAST:event_hapusBtnActionPerformed
 
     /**
      * @param args the command line arguments
